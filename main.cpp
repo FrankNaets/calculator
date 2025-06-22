50d1b230f4daa67a8e97f8bb8d848b95d8e938fb
FrankNaets <frank.naets@gmail.com>
2025-06-22 09:06:55 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    double x;
+    double y;
+  cout << "Type your first number: "; // Type a number and press enter
+  cin >> x;// Get user input from the keyboard
+  cout << "Type your second number: "; // Type a number and press enter
+  cin >> y; // Get user input from the keyboard
+  cout << "Your result with plus is: " << x + y << endl; // Output the sum of the two numbers
+  cout << "Your result with minus is: " << x - y << endl; // Output the difference of the two numbers
+  cout << "Your result with multiply is: " << x * y << endl; // Output the difference of the two numbers
+  cout << "Your result with divide is: " << x / y << endl; // Output the difference of the two numbers
+  cout << "To close press zero and then enter "; // Type a number and press enter
+  cin >> x;// Get user input from the keyboard
+  return 0;
+
+}
